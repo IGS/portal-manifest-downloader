9733f1a457639c599f6ebbb6c7b761e382b51e65
mschor <michael.a.schor@gmail.com>
2018-10-31 11:10:30 -0400

    Updates for oauth

--- bin/get_dcppc_manifest.py
+++ bin/get_dcppc_manifest.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
 import argparse
-import sys
 import requests
-import getpass
+import json
+import tempfile
+import sys
+from oauth2client import tools
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
 
 """DCPPC manifest Downloader
 
@@ -15,7 +19,7 @@ cloud during use of that website.
 
 Example:
 
-        $ python -m <manifest-id> -u <username> get_dcppc_manifest.py
+        $ python get_dcppc_manifest.py -m <manifest-id> 
 
 Note: The manifest-id is issued by the portal.nihdatacommons.us site and
 presented to the user at the time the manifest file is saved to the cloud.
@@ -52,42 +56,76 @@ def query_yes_no(question, default="no"):
             sys.stdout.write("Please respond with 'yes' or 'no' "
                              "(or 'y' or 'n').\n")
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-m", "--manifest-id", help="ID that was presented when the Manifest file was sent to S3")
-parser.add_argument("-u", "--username", help="Your username for accessing https://portal.nihdatacommons.us/")
-args = parser.parse_args()
+MANIFEST_URL = "http://portal-staging.nihdatacommons.us/api/manifest/download?id="
 
-if not (args.manifest_id and args.username):
-    print("Must supply id and username")
-    parser.print_help(sys.stderr)
-    sys.exit(1)
 
-prompt="Do you agree that you are executing this in a cloud environment per the policies of the DCPPC and also that you have read and agree to the NIH data access policy (https://gdc.cancer.gov/access-data/data-access-policies)?"
+myparser = argparse.ArgumentParser(description="Obtain the portal manifest by ID.")
+myparser.add_argument('-m', '--manifest',
+                    required=True,
+                    help="Specify the manifest ID issued by the Portal.")
+myparser.add_argument('--logging_level', default='ERROR',
+                      choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+                      help='Set the logging level of detail.')
+
+# Parse the arguments from the CLI
+args = myparser.parse_args()
+
+prompt="\nDo you agree that you are executing this in a cloud environment per the policies \n" + \
+       "of the DCPPC and also that you have read and agree to the NIH data access policy \n" + \
+       "at https://gdc.cancer.gov/access-data/data-access-policies?"
 
 confirmation = query_yes_no(prompt)
+if not confirmation:
+    sys.exit(0)
 
-print ("\nPlease enter your DCPPC Password")
-pw = getpass.getpass()
+# Generate a temporary file. A file is required when instantiating
+# a Storage object, which we need to later invoke oauth's run_flow()
+temp_file = tempfile.NamedTemporaryFile(delete=True).name
+storage = Storage(temp_file)
 
-filename = 'dcppc_manifest_' + args.manifest_id + '.tsv'
-url = 'http://portal.nihdatacommons.us/api/manifest?id=' + filename
+# Setup the scope of what to access...
+SCOPE = 'https://www.googleapis.com/auth/userinfo.profile'
+
+flow = flow_from_clientsecrets('client_id.json', scope=SCOPE)
+
+# Force the assumption that there is a headless environment and 
+# that a graphical browser and local webserver are NOT available.
+args.noauth_local_webserver=True
+
+# Get the manifest ID from what the user specified on the CLI.
+manifest_id = args.manifest
+
+# Now we create our Credentials object (for which we needed the temporary
+# file and command line arguments).
+credentials = tools.run_flow(flow, storage, args)
+
+# Grab the necessary tokens
+token_response = credentials.token_response
+access_token = token_response['access_token']
+id_token = token_response['id_token']
+
+headers = {'Authorization': 'Bearer {}'.format(access_token)}
+
+print("---------------------------------------------")
+
+# Assume a regular manifest file using the given ID.
+
+filename = 'dcppc_manifest_' + manifest_id + '.tsv'
 print ("Fetching manifest...")
+response = requests.get(MANIFEST_URL + filename, headers=headers)
 
-response = requests.get(url, auth=requests.auth.HTTPBasicAuth(args.username, pw))
-
+# if no file returned, try a manifest metadata file with the given ID
 if response.status_code == 404 or response.status_code == 500:
-    filename = 'dcppc_manifest_metadata_' + args.manifest_id + '.tsv'
-    url = 'http://portal.nihdatacommons.us/api/manifest?id=' + filename
-    response = requests.get(url, auth=requests.auth.HTTPBasicAuth(args.username, pw))
+    filename = 'dcppc_manifest_metadata_' + manifest_id + '.tsv'
+    response = requests.get(MANIFEST_URL + filename, headers=headers)
 
 if response.status_code != 200:
     if response.status_code == 404:
         print("No manifest was found with the given ID")
     else:
-        print("An HTTP response code of " + str(response.status_code) + " was returned")
+        print("HTTP error " + str(response.status_code) + " was retuned")
         print(response.content)
 else:
     with open(filename, 'wb') as f:
         f.write(response.content)
-    print ("Done.")
-
+        print("Manifest saved to {}.".format(filename))
